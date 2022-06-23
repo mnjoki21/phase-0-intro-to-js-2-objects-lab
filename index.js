d4896d2b99bf7a3d3a433c63fb157389900525ad
mitchelle <mitchellenjoki7@gmail.com>
2022-06-23 20:13:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
- const employee = { 
-     
-         name : 'Sam'
-     }
-emplloyee.name;
 
- 
    
    
-   
-   
-   
-   function updateEmployeeWithKeyAndValue (employee , key , value ) {  
-function updateDriverWithKeyAndValue (driver , key , value)
+   const employee = {
+    name: "sam",
+    streetAddress: "broadway 11"
 }
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObject = {...employee};
+    newObject[key] = value;
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    console.log(employee)
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+} 
+
 
